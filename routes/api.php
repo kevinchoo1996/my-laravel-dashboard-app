18fab531094fb3d46d46e9459b2fe2a30c512367
kevinchoo1996 <45227763+kevinchoo1996@users.noreply.github.com>
2026-01-12 20:39:16 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::prefix('inventory')->name('inventory.')->group(function () {
+    Route::prefix('inventory')->name('api-inventory.')->group(function () {
             Route::delete('products/bulk-delete', [ProductController::class, 'bulkDelete'])->name('products.bulk-delete');
             Route::apiResource('products', ProductController::class);
     });
